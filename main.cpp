c6fd1f41a8d9c36c001c45a73528a42cc6b2b4af
DoryGuy <gwpowell@hotmail.com>
2023-12-19 11:24:31 -0800

    Use constexpr and consteval

--- main.cpp
+++ main.cpp
@@ -14,7 +14,7 @@
 
 // Solution is O(N) it uses minimal space
 template<class T>
-int getVolume (T const &elevation) {
+consteval T::value_type getVolume (T const &elevation) {
     if (elevation.empty() ) {
         return 0;
     }
@@ -22,8 +22,8 @@ int getVolume (T const &elevation) {
     typename T::value_type  max_right_side(0);
     typename T::value_type  volume(0);
     
-    auto left_side_iter(elevation.begin());
-    auto right_side_iter(elevation.rbegin());
+    auto left_side_iter(elevation.cbegin());
+    auto right_side_iter(elevation.crbegin());
     
     while (left_side_iter <= (right_side_iter + 1).base()) {
         if (*left_side_iter < *right_side_iter) {
@@ -54,47 +54,47 @@ int main() {
     using std::endl;
     using std::array;
     
-    array<int, 12> elevation{0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1};
+    static constexpr array<int, 12> elevation{0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1};
     auto volume = getVolume(elevation);
     cout << "Volume Original Problem = " << volume << endl;
     assert(volume == 6);
     
-    array<int, 12> sea_level_elevation{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    static constexpr array<int, 12> sea_level_elevation{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
     volume = getVolume(sea_level_elevation);
     cout << "Volume Sea level = " << volume << endl;
     assert(volume == 0);
     
-    array<int, 12> mountain_elevation{0, 1, 2, 3, 4, 5, 5, 4, 3, 2, 1, 0};
+    static constexpr array<int, 12> mountain_elevation{0, 1, 2, 3, 4, 5, 5, 4, 3, 2, 1, 0};
     volume = getVolume(mountain_elevation);
     cout << "Volume Mountain = " << volume << endl;
     assert(volume == 0);
     
-    array<int, 12> valley_elevation{5, 4, 3, 2, 1, 0, 0, 1, 2, 3, 4, 5};
+    static constexpr array<int, 12> valley_elevation{5, 4, 3, 2, 1, 0, 0, 1, 2, 3, 4, 5};
     volume = getVolume(valley_elevation);
     cout << "Volume Valley = " << volume << endl;
     assert(volume == 30);
     
-    array<int, 12> high_plains_elevation{5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5};
+    static constexpr array<int, 12> high_plains_elevation{5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5};
     volume = getVolume(high_plains_elevation);
     cout << "Volume High Plains = " << volume << endl;
     assert(volume == 0);
     
-    array<int, 0> empty_container{};
+    static constexpr array<int, 0> empty_container{};
     volume = getVolume(empty_container);
     cout << "Volume Empty Container = " << volume << endl;
     assert(volume == 0);
 
-    array<int, 1> one_element_container{1};
+    static constexpr array<int, 1> one_element_container{1};
     volume = getVolume(one_element_container);
     cout << "Volume One element Container = " << volume << endl;
     assert(volume == 0);
 
-    array<int, 2> two_element_container{1, 1};
+    static constexpr array<int, 2> two_element_container{1, 1};
     volume = getVolume(two_element_container);
     cout << "Volume Two element Container = " << volume << endl;
     assert(volume == 0);
     
-    array<int, 3> three_element_container{1, 0, 1};
+    static constexpr array<int, 3> three_element_container{1, 0, 1};
     volume = getVolume(three_element_container);
     cout << "Volume Three element Container = " << volume << endl;
     assert(volume == 1);
